2b80d7aed57c51ed2b511a74937c6aa46988d69f
erfgh2948 <71313806+erfgh2948@users.noreply.github.com>
2023-02-27 14:02:55 +0900

    Update AccountController.java

--- src/main/java/com/ex/backend/controller/AccountController.java
+++ src/main/java/com/ex/backend/controller/AccountController.java
@@ -59,7 +59,7 @@ public class AccountController {
 			gc3.setHttpOnly(true);//자바스크립트로 접근불가능
 			gc3.setPath("/");//모든경로에서 접근가능
             //gc3.setDomain("https://web-wine-frontend-4uvg2mledushse.sel3.cloudtype.app");
-			gc3.setDomain("https://port-0-wine-backend-4uvg2mledushse.sel3.cloudtype.app");
+			//gc3.setDomain("https://port-0-wine-backend-4uvg2mledushse.sel3.cloudtype.app");
 			
 			res.addCookie(gc3);//서블릿 res에 쿠키추가
 			
